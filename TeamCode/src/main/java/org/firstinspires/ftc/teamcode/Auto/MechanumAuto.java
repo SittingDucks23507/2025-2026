6d74fa07fc262b772c7a97143603957a092c2841
ccf <eryckdg@hotmail.com>
2025-12-02 12:54:06 -0600

    team initial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/MechanumAuto.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_TO_POSITION;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class MechanumAuto {
+    public LinearOpMode lop;
+    // Directions to be used with movetime()
+    public final int[][] FORWARD = { {1, 1},
+            {1, 1} };
+    public final int[][] LEFT    = { {-1, 1},
+            {1, -1} };
+
+    public final int[][] DOWN    = dtoim(mulsm(FORWARD, -1));
+    public final int[][] RIGHT   = dtoim(mulsm(LEFT, -1));
+
+    // define motors
+    public DcMotor frontLeft, frontRight, backLeft, backRight;
+    // define runtime, used in movetime()
+    private final ElapsedTime runtime = new ElapsedTime();
+
+    // Motor SPEED, adjust as necessary
+    private final float SPEED = 0.25f;
+    private final float TPR = ((((1+(46.0f/17.0f))) * (1+(46.0f/11.0f))) * 28); // TODO: Update to actual value
+    private final float RPI = 1.0f; // TODO: find out
+
+    // Takes an integer matrix ‘m’ and multiplies it by scalar ‘c’
+    private double[][] mulsm(int[][] m, double c) {
+        double[][] r = new double[2][2];
+        r[0][0] = m[0][0] * c;
+        r[0][1] = m[0][1] * c;
+        r[1][0] = m[1][0] * c;
+        r[1][1] = m[1][1] * c;
+
+        return r;
+    }
+    // Converts double[][] to int[][]
+    // Should the directions just be doubles? Probably.
+    private int[][] dtoim(double[][] m) {
+        int[][] r = new int[2][2];
+        r[0][0] = (int)m[0][0];
+        r[0][1] = (int)m[0][1];
+        r[1][0] = (int)m[1][0];
+        r[1][1] = (int)m[1][1];
+
+        return r;
+    }
+
+    // Runs motors at SPEED power for ‘time’ seconds in ‘direction’
+    // Directions as defined above ‘FORWARD’ ‘DOWN’ ‘LEFT’ ‘RIGHT’
+    public void movetime(float time, int[][] direction) {
+        double[][] movevec = mulsm(direction, SPEED);
+
+        frontLeft.setPower(movevec[0][0]);
+        frontRight.setPower(movevec[0][1]);
+        backLeft.setPower(movevec[1][0]);
+        backRight.setPower(movevec[1][1]);
+
+        runtime.reset();
+        while (lop.opModeIsActive() && (runtime.seconds() < time)) {
+            lop.telemetry.addData("time", runtime.seconds());
+            lop.telemetry.update();
+        }
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+    public void moveticks(float inches, int[][] direction) {
+        frontLeft.setTargetPosition((int)(inches * TPR * RPI) * direction[0][0]);
+        frontRight.setTargetPosition((int)(inches * TPR * RPI) * direction[0][1]);
+        backLeft.setTargetPosition((int)(inches * TPR * RPI) * direction[1][0]);
+        backRight.setTargetPosition((int)(inches * TPR * RPI) * direction[1][1]);
+
+        frontLeft.setMode(RUN_TO_POSITION);
+        frontRight.setMode(RUN_TO_POSITION);
+        backLeft.setMode(RUN_TO_POSITION);
+        backRight.setMode(RUN_TO_POSITION);
+        frontLeft.setPower(SPEED);
+        frontRight.setPower(SPEED);
+        backLeft.setPower(SPEED);
+        backRight.setPower(SPEED);
+        while (lop.opModeIsActive() &&
+                frontLeft.isBusy() &&
+                frontRight.isBusy() &&
+                backLeft.isBusy() &&
+                backRight.isBusy()) {
+            lop.telemetry.addData("Target (FLFRBLBR)", "%7d, %7d, %7d, %7d",
+                    frontLeft.getTargetPosition(),
+                    frontRight.getTargetPosition(),
+                    backLeft.getTargetPosition(),
+                    backRight.getTargetPosition());
+
+            lop.telemetry.addData("Position (FLFRBLBR)", "%7d, %7d, %7d, %7d",
+                    frontLeft.getCurrentPosition(),
+                    frontRight.getCurrentPosition(),
+                    backLeft.getCurrentPosition(),
+                    backRight.getCurrentPosition());
+            lop.telemetry.update();
+        }
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,122 @@
+/* Copyright (c) 2025 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Config;
+
+/*
+ * This OpMode illustrates how to program your robot to drive field relative.  This means
+ * that the robot drives the direction you push the joystick regardless of the current orientation
+ * of the robot.
+ *
+ * This OpMode assumes that you have four mecanum wheels each on its own motor named:
+ *   front_left_motor, front_right_motor, back_left_motor, back_right_motor
+ *
+ *   and that the left motors are flipped such that when they turn clockwise the wheel moves backwards
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ *
+ */
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "first", group = "Robot")
+public class TeleOp extends OpMode {
+    // This declares the four motors needed
+    DcMotor frontLeft, frontRight, backLeft, backRight;
+    DcMotor spinny;
+    DcMotorEx theCoolerSpinny;
+    CRServo left_assist, right_assist;
+    ElapsedTime assist_timer = new ElapsedTime();
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right");
+        backLeft = hardwareMap.get(DcMotor.class, "back_left");
+        backRight = hardwareMap.get(DcMotor.class, "back_right");
+        spinny = hardwareMap.get(DcMotor.class, "spinny");
+        theCoolerSpinny = (DcMotorEx)spinny;
+
+        left_assist = hardwareMap.get(CRServo.class, "left_assist");
+        right_assist = hardwareMap.get(CRServo.class, "right_assist");
+
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        spinny.setDirection(DcMotorSimple.Direction.REVERSE);
+        right_assist.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void loop() {
+        double lsY, lsX, rsX;
+        float speed = 1f;
+        float assist_power;
+        spinny.setPower(.96*Config.spinny_speed);
+
+        if (gamepad1.right_trigger > 0.1) {
+            speed = 0.25f;
+        }
+
+        /* if (gamepad1.left_bumper) {
+            spinny.setPower(Config.spinny_speed);
+        } */
+
+        if (gamepad1.rightBumperWasPressed()) {
+            assist_timer.reset();
+        }
+        if (assist_timer.seconds() < Config.secs) {
+            assist_power = 0.3f;
+        } else {
+            assist_power = 0.0f;
+        }
+        left_assist.setPower(assist_power);
+        right_assist.setPower(assist_power);
+
+        lsY = -gamepad1.left_stick_y; // W_UP is now positive
+        lsX = gamepad1.left_stick_x;
+        rsX = gamepad1.right_stick_x; // un-backwards it
+        frontLeft.setPower((lsY + lsX + rsX)  * speed);
+        frontRight.setPower((lsY - lsX - rsX) * speed);
+        backLeft.setPower((lsY - lsX + rsX)   * speed);
+        backRight.setPower((lsY + lsX - rsX)  * speed);
+        // 28 ticks per revolution
+        telemetry.addData("spinny tps", (theCoolerSpinny.getVelocity()));
+        telemetry.update();
+    }
+}
